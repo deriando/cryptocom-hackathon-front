038b51b9a15e3d3ea894be7d975959fa3f0e0be2
deriando <10672809+deriando@users.noreply.github.com>
2022-10-07 20:49:20 +0800

    added button to connect MetaMask wallet

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
-import { useState } from "react";
+import { createContext, useContext, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 import { Button } from "@mui/material";
+import { ethers } from "ethers";
+
+// A Web3Provider wraps a standard Web3 provider, which is
+// what MetaMask injects as window.ethereum into each page
+// @ts-ignore window.ethereum type not available
+const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+// MetaMask requires requesting permission to connect users accounts
+async function connectWallet() {
+  await provider.send("eth_requestAccounts", []);
+}
+// The MetaMask plugin also allows signing transactions to
+// send ether and pay to change state within the blockchain.
+// For this, you need the account signer...
+const signer = provider.getSigner();
 
 function App() {
   const [count, setCount] = useState(0);
@@ -18,8 +33,8 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <Button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <Button id="wallet-connect" onClick={connectWallet}>
+          Wallet Connect
         </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
