6afa664942758253ce0ae5f46b2ec3265cf83487
Sky Livingston <27288135+skypad123@users.noreply.github.com>
2022-10-13 02:05:18 +0800

    Added ERC20 Interface

---
+++ src/logic/ERC20.ts
@@ -0,0 +1,53 @@
+import {
+  ethers,
+  BigNumber,
+  Contract,
+  providers,
+  Signer,
+  Transaction,
+} from "ethers";
+
+import ERC20Meta from "../../artifacts/@openzeppelin/contracts/token/ERC20/ERC20.sol/ERC20.json" assert { type: "json" };
+
+interface ERC20Interface {
+  defaultCaller: Signer;
+  defaultProvider: providers.Provider;
+  _ERC20Contract: Contract | null;
+}
+
+class ERC20Interface {
+  constructor(defaultCaller: Signer, defaultProvider: providers.Provider) {
+    this.defaultCaller = defaultCaller;
+    this.defaultProvider = defaultProvider;
+    this._ERC20Contract = null;
+  }
+
+  async setContract(
+    this: ERC20Interface,
+    directDonationAddress: string,
+    provider = this.defaultProvider
+  ) {
+    const meta = ERC20Meta;
+    this._ERC20Contract = await new ethers.Contract(
+      directDonationAddress,
+      meta.abi
+    ).connect(provider);
+  }
+
+  async symbol(
+    this: ERC20Interface,
+    address: string,
+    caller = this.defaultCaller
+  ): Promise<string> {
+    this._isContractSet();
+    return await (this._ERC20Contract as Contract).connect(caller).symbol();
+  }
+
+  _isContractSet(this: ERC20Interface) {
+    if (this._ERC20Contract === null)
+      throw () =>
+        new Error(
+          "Contract is not set. Use setContract function to set contract address."
+        );
+  }
+}
